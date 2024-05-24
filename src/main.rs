80780b95bf97ed3198055d02cbd3af8fd5089392
Konstantinos Benos <mpenos.ks@gmail.com>
2024-05-24 18:52:05 +0300

    Create template ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+const IMG_WIDTH: u16 = 512;
+const IMG_HEIGHT: u16 = 512;
+
 fn main() {
-    println!("Hello, world!");
+    print_image_header();
+    
+    let mult_w: f32 = (256 as f32 / IMG_WIDTH as f32) as f32;
+    let mult_h: f32 = (256 as f32 / IMG_HEIGHT as f32) as f32;
+    
+    for i in 0..IMG_WIDTH {
+        for j in 0..IMG_HEIGHT {
+            let r = (j as f32 * mult_w) as u32;
+            let g = (i as f32 * mult_h) as u32;
+            let b = 0;
+            
+            println!("{r} {g} {b}");
+        }
+    }
+}
+
+fn print_image_header() {
+    println!("P3");
+    println!("{IMG_WIDTH} {IMG_HEIGHT}");
+    println!("255");
 }
