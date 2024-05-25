dfe690a216b004106ab5776b06f968362c3483b7
Konstantinos Benos <mpenos.ks@gmail.com>
2024-05-25 23:43:38 +0300

    Add dot/cross and normalized functions

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,25 @@ impl Vec3 {
         
         squared.sqrt()
     }
+    
+    fn normalized(&self) -> Vec3 {
+        let inv_len = 1f64 / self.len();
+        
+        Vec3 {x: self.x * inv_len, y: self.y * inv_len, z: self.z * inv_len}
+    }
+}
+
+fn dot(a: Vec3, b: Vec3) -> f64 {
+    a.x * b.x + a.y * b.y + a.z * b.z
+}
+
+fn cross(a: Vec3, b: Vec3) -> Vec3 {
+    
+    Vec3 { 
+        x: a.y * b.z - b.y * a.z,
+        y: a.x * b.z - b.x * a.z,
+        z: a.x * b.y - b.x * a.y
+    }
 }
 
 fn main() {
