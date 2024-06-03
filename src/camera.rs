cd9a8ce52e1140b71a2e1f5343c5a40185b2f6fa
Konstantinos Benos <mpenos.ks@gmail.com>
2024-06-03 19:11:13 +0300

    Delete camera new method

--- src/camera.rs
+++ src/camera.rs
@@ -21,46 +21,38 @@ pub struct Camera {
     pub pixel_delta_v: Vec3
 }
 
-impl Camera {
-    pub fn new() -> Self {
-        Camera {
-            position: Vec3 {x: 0.0, y: 0.0, z: 0.0},
-            aspect_ratio: 1.0,
-            frame_width: 1024,
-            frame_height: 1024,
-            frame_res: 1024 * 1024,
-            focal_length: 1.0,
-            view_height: 2.0,
-            view_width: 2.0,
-            view_u: Vec3 {x: 2.0, y: 0.0, z: 0.0},
-            view_v: Vec3 {x: 0.0, y: -2.0, z: 0.0},
-            pixel_zero: Vec3 {x: 0.0, y: 0.0, z: 0.0},
-            pixel_delta_u: Vec3 {x: 0.0, y: 0.0, z: 0.0},
-            pixel_delta_v: Vec3 {x: 0.0, y: 0.0, z: 0.0},
-            view_pixel_upper_left: Vec3 {x: 0.0, y: 0.0, z: 0.0},
-        }
-    }
-}
-
 impl Default for Camera {
     fn default() -> Self {
-        let mut camera: Camera = Camera::new();
+        let position = Vec3 {x: 0.0, y: 0.0, z: 0.0};
+        let aspect_ratio = 16.0 / 9.0;
+        let frame_width = 1024;
+        let frame_height = (frame_width as f64 / aspect_ratio) as u32;
+        let frame_res = frame_width * frame_height;
+        let focal_length = 1.0;
+        let view_height = 2.0;
+        let view_width = view_height * (frame_width as f64 / frame_height as f64);
+        let view_u = Vec3::new(view_width, 0.0, 0.0);
+        let view_v = Vec3::new(0.0, -view_height, 0.0);
+        let pixel_delta_u = view_u * (1.0 / frame_width as f64);
+        let pixel_delta_v = view_v * (1.0 / frame_height as f64);
+        let view_pixel_upper_left = position - Vec3::new(0.0, 0.0, focal_length) - (view_u + view_v) * 0.5;
+        let pixel_zero = view_pixel_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
         
-        camera.aspect_ratio = 16.0 / 9.0;
-        camera.frame_width = 1024;
-        camera.frame_height = (camera.frame_width as f64 / camera.aspect_ratio) as u32;
-        camera.frame_res = camera.frame_width * camera.frame_height;
-        
-        camera.focal_length = 1.0;
-        camera.view_height = 2.0;
-        camera.view_width = camera.view_height * (camera.frame_width as f64 / camera.frame_height as f64);
-        camera.view_u = Vec3::new(camera.view_width, 0.0, 0.0);
-        camera.view_v = Vec3::new(0.0, -camera.view_height, 0.0);
-        camera.pixel_delta_u = camera.view_u * (1.0 / camera.frame_width as f64);
-        camera.pixel_delta_v = camera.view_v * (1.0 / camera.frame_height as f64);
-        camera.view_pixel_upper_left = camera.position - Vec3::new(0.0, 0.0, camera.focal_length) - (camera.view_u + camera.view_v) * 0.5;
-        camera.pixel_zero = camera.view_pixel_upper_left + (camera.pixel_delta_u + camera.pixel_delta_v) * 0.5;
-        
-        camera
+        Camera {
+            position,
+            aspect_ratio,
+            frame_width,
+            frame_height,
+            frame_res,
+            focal_length,
+            view_height,
+            view_width,
+            view_u,
+            view_v,
+            pixel_zero,
+            pixel_delta_u,
+            pixel_delta_v,
+            view_pixel_upper_left,
+        }
     }
 }
