c89bc4040a655ba168e12d486c18d1db0cea9f88
Konstantinos Benos <mpenos.ks@gmail.com>
2024-06-05 15:48:10 +0300

    Add function to calculate random vector on hemisphere

--- src/rt_util.rs
+++ src/rt_util.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use crate::Vec3;
+use crate::vector::{Vec3, dot};
 
 pub const PI: f64 = 3.1415926535897932385;
 pub const INFINITY: f64 = f64::INFINITY;
@@ -27,4 +27,15 @@ pub fn random_in_unit_sphere() -> Vec3 {
             return point;
         }
     }
+}
+
+pub fn random_on_unit_hemisphere(normal: &Vec3) -> Vec3 {
+    let point = random_in_unit_sphere().normalized();
+    let is_correct_hs = dot(normal, &point) > 0.0;
+    
+    if is_correct_hs {
+        return point;
+    }
+    
+    point * -1.0
 }
